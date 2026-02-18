807a4bce0c05ef7d8c02da03dd03b0ca7d9ee032
Adil Nas <adilnasceng@gmail.com>
2026-02-18 19:58:48 +0300

    first commit

---
+++ build/mysimrobot/CMakeFiles/mysimrobot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mysimrobot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mysimrobot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
